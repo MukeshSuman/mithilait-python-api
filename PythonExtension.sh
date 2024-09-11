200d58846b9122a93c17a6de1c1d99fdcd659208
Mukesh Suman <sumanm686@gmail.com>
2024-09-11 21:04:01 +0530

    Remove __pycache__ folder

---
+++ PythonExtension.sh
@@ -0,0 +1,25 @@
+ext install ms-python.python
+ext install ms-python.vscode-pylance
+ext install ms-python.black-formatter
+# ext install ms-python.flake8
+ext install ms-pyright.pyright
+ext install ms-python.vscode-pylance
+ext install ms-python.isort
+ext install ms-python.vscode-pylance
+ext install ms-toolsai.jupyter
+ext install esbenp.prettier-vscode
+
+# settings.json:
+# {
+#     "python.pythonPath": "path/to/your/python", // Ensure this points to your Python interpreter
+#     "python.linting.enabled": true,
+#     "python.linting.flake8Enabled": true,
+#     "python.linting.flake8Path": "flake8",
+#     "python.formatting.provider": "black",
+#     "python.formatting.blackPath": "black",
+#     "editor.formatOnSave": true,
+#     "python.analysis.typeCheckingMode": "basic",
+#     "python.sortImports.args": ["--profile", "black"],
+#     "python.languageServer": "Pylance"
+# }
+
---
+++ crud.sh
@@ -0,0 +1,5 @@
+mkdir app/user
+touch app/user/__init__.py
+touch app/user/models.py
+touch app/user/schemas.py
+touch app/user/routes.py
